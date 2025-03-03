d930bcb6d98aa2e7a98368e3ffe75d4ed44edb8c
Mariana Amy <MartinezNevarezMarianaA@JohnDeere.com>
2025-03-03 12:11:01 -0600

    Hook

---
+++ src/app/hooks/fetchUser.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export const useFetchData = (url : string) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json)
+        setData(json);
+      } catch (error) {
+        setError(error)
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData()
+
+  }, [url]);
+
+  //return necessary data
+
+  return { data, loading, error}
+};
